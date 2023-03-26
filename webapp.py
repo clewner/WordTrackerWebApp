1e88766d4a875ca62acd84e88a35d794e35e878b
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-03-26 10:45:39 -0700

    k

--- webapp.py
+++ webapp.py
@@ -79,3 +79,6 @@ def index():
 
     else:
         return render_template('home.html', enteredbook = "", enteredwords = "", paragraphs = "")
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0')
