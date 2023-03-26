a252325f8770a3989628a9dfc362d5a60b4b13d6
Isaac Saxonov <isaac.saxonov@gmail.com>
2023-03-26 00:34:32 -0700

    asd

---
+++ webapp.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import jinja2
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        
+        
+        
+        bookname = request.form.get("bookname")
+        bookname = bookname.replace(" ", "+")
+
+        bookListUrl = "https://www.gutenberg.org/ebooks/search/?query=" + bookname.lower() + "&submit_search=Go%21"
+        bookListPage = requests.get(bookListUrl)
+        soupListPage = BeautifulSoup(bookListPage.content, "html.parser")
+        if(soupListPage.find(class_="booklink")):
+            bookTypeUrl = "https://www.gutenberg.org" + soupListPage.find(class_="booklink").find('a')['href']
+            bookTypePage = requests.get(bookTypeUrl)
+            soupTypePage = BeautifulSoup(bookTypePage.content, "html.parser")
+
+            URL = "https://www.gutenberg.org" + soupTypePage.find('a', string='Read this book online: HTML5')['href']
+            page = requests.get(URL)
+            soup = BeautifulSoup(page.content, "html.parser")
+
+
+            wordlist = request.form.get("wordlist")
+            wordlistlist = wordlist.split('-')
+
+            results = soup.findAll("p")
+
+            paragraphlist = []
+
+            for x in results:
+                countstring = 0
+                for j in wordlistlist:
+                    if re.search(r"\b" + j + r"\b", x.text):
+                        countstring += 1
+                
+                if(countstring > 0):
+                    tempstring = ""
+                    if(x.find_previous_sibling(["h2", "h3"])):
+                        tempstring = x.find_previous_sibling(["h2", "h3", "h4"]).text
+                    else:
+                        tempstring = "None"
+                    
+                    
+
+                    actualtext = x.text
+                    pattern = r'\[\d+\]'
+
+                    actualtext = re.sub(pattern, '', actualtext)
+                    tempstring = tempstring + "<br>" + actualtext
+
+                    paragraphlist.append(tempstring)
+                
+
+        
+
+            return render_template('home.html', enteredbook = bookname.replace("+", " "), enteredwords = wordlist, paragraphs = paragraphlist)
+        else:
+            error = ["Couldn't find book"]
+            return render_template('home.html', enteredbook = request.form.get("bookname"), enteredwords = request.form.get("wordlist"), paragraphs = error)
+        
+
+    else:
+        return render_template('home.html', enteredbook = "", enteredwords = "", paragraphs = "")
